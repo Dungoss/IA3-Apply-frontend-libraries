c7014de811743283c016e15076151beb608017a7
Dungoss <70448211+Dungoss@users.noreply.github.com>
2022-11-02 09:49:15 +0700

    apply react-router

---
+++ src/Fetch.js
@@ -0,0 +1,9 @@
+const Fetch = () => {
+    return (
+        <div className="create">
+            <h2>Fetch page</h2>
+            </div>
+    )
+}
+
+export default Fetch;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="create">
+            <h2>Home page</h2>
+            </div>
+    )
+}
+
+export default Home;
